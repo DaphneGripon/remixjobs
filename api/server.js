4776f13ea98a9cb3da6f9ad072d06685ea0d60db
Daphne GRIPON <daphne.gripon@gmail.com>
2015-12-14 18:01:16 +0100

    Ajout des requêtes API sur /jobs, /jobs/:id, /jobs/latest et /companies. Non implémentées.

--- api/server.js
+++ api/server.js
@@ -1,33 +1,81 @@
 var express = require('express');
-var router = express.Router();
+var remixjobs = express.Router();
+var util = require('util');
 var app = express();
+var bodyParser = require('body-parser');
 var Student = require('./models/students');
 var mongoose = require('mongoose');
-var ObjectID = require('mongodb').ObjectID
+var ObjectID = require('mongodb').ObjectID;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 //Lets connect to our database using the DB server URL.
 mongoose.connect('mongodb://localhost/remixjobs');
 
 // test route to make sure everything is working
-router.get('/', function(req, res) {
+remixjobs.get('/', function(req, res) {
     res.json({
-      message: 'Welcome to Students API'
+      message: 'Welcome to Students API',
     });
 });
 
-router.route('/students')
+remixjobs.route('/students')
   .post(function(req, res) {
+    var body = req.body;
     var student = new Student();
     var s_id = new ObjectID();
+    var fieldsValid = checkFields(req.body);
+
     student._id = s_id;
-    student.name = "daphne";
-    student.intern = "hallo";
-    student.grade = "20";
-    student.save(function(err) {
+    if(body != undefined)
+    {
+      student.name = req.body.name;
+      student.intern = req.body.intern;
+      student.grade = req.body.grade;
+    }
+
+    var student_info = {
+      "name" : student.name == undefined ? "Undefined" : student.name,
+      "intern" : student.intern == undefined ? "Undefined" : student.intern,
+      "grade" : student.grade == undefined ? "Undefined" : student.grade,
+    };
+
+    // console.log("student_info:"+util.inspect(student_info, {showHidden: false, depth: null}));
+
+    if(!fieldsValid)
+    {
+      res.status(400).json(
+        {
+          message: 'No student created. You did not send enough information to create that student.',
+          data: student_info
+        }
+      );
+    }
+    else {
+        student.save(function(err) {
+          if (err) {
+            res.send(err);
+          }
+          res.status(201).json(
+            {
+              'message': 'Student created!',
+              'id': s_id,
+              'student_info': student_info==undefined?'undefined':student_info,
+            }
+          );
+        });
+    }
+  } )
+  .delete(function(req, res) {
+    Student.remove(function(err) {
       if (err) {
         res.send(err);
       }
-      res.json({ message: 'Student created!', id: s_id});
+      res.json({ message: 'Students removed!' });
     });
   })
   .get(function(req, res) {
@@ -40,7 +88,7 @@ router.route('/students')
     });
   });
 
-router.route('/students/:student_id')
+remixjobs.route('/students/:student_id')
 .get(function(req, res) {
   Student.findById(req.params.student_id, function(err, student) {
       if (err){
@@ -50,11 +98,80 @@ router.route('/students/:student_id')
   });
 });
 
+remixjobs.route('/jobs')
+.get(function(req, res) {
+  var message =
+  {
+    'message' : 'Not yet implemented'
+  };
+  res.status(501).json(message);
+})
+.post(function(req, res) {
+  var message =
+  {
+    'message' : 'Not yet implemented'
+  };
+  res.status(501).json(message);
+})
+.put(function(req, res) {
+  var message =
+  {
+    'message' : 'Not yet implemented'
+  };
+  res.status(501).json(message);
+});
+
+remixjobs.route('/jobs/:job_id')
+.get(function(req, res) {
+  var message =
+  {
+    'message' : 'Not yet implemented'
+  };
+  res.status(501).json(message);
+});
+
+remixjobs.route('/jobs/latest')
+.get(function(req, res) {
+  var message =
+  {
+    'message' : 'Not yet implemented'
+  };
+  res.status(501).json(message);
+});
+
+remixjobs.route('/companies')
+.get(function(req, res) {
+  var message =
+  {
+    'message' : 'Not yet implemented'
+  };
+  res.status(501).json(message);
+});
+
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', remixjobs);
 app.listen(3000, function(){
 	console.log('listening on port *:3000');
 });
+
+function checkFields(body)
+{
+    var OK = true;
+    if(body == null || body == undefined)
+    {
+      OK = false;
+    }
+    else
+    {
+      if(body.name == null || body.name == undefined)
+        OK = false;
+      if(body.grade == null || body.grade == undefined)
+        OK = false;
+      if(body.intern == null || body.intern == undefined)
+        OK = false;
+    }
+    return OK;
+}
